fd57a23d82219cc26a55fc101c743b89cf1eadb9
Konstantin Perikov <pericov.ko@gmail.com>
2022-05-23 14:08:39 +0100

    add how much solution

--- how-much/how_much.py
+++ how-much/how_much.py
@@ -1,6 +1,18 @@
 import pickle
-from tensorflow import keras
+import pandas as pd
 
 with open("model_ex.pkl", "rb") as f:
-    model = pickle.load(f)
-    print(model)
+    source_model = pickle.load(f)
+    print(source_model.summary())
+
+inner = source_model.layers[0]
+print(inner.summary())
+
+df = pd.read_csv("objects.csv")
+print(df.head)
+
+for _, row in df.iterrows():
+    x = row.iloc[1:].to_numpy()
+    x = x.reshape(1, -1)
+    print(x)
+    print(inner.predict(x))
